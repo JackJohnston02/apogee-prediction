f201ffe8b86b7c4df08235e9d4465f4841f11416
JackJohnston02 <jackjohnston1472@gmail.com>
2024-05-10 23:30:45 +0100

    add airbrake animation function

--- CARFS-24/plot_data.m
+++ CARFS-24/plot_data.m
@@ -1,8 +1,10 @@
 
 plot_dynamics = true;
 plot_control_output = true;
+animation_airbrake_position = true; 
 
 
+%% Dynamics output
 if plot_dynamics == true
     dynamics_fig = figure;
     % Make plots appear fulscreen
@@ -35,6 +37,7 @@ if plot_dynamics == true
     grid on;  % add a grid
 end
 
+%% Control output
 if plot_control_output == true
     
     control_output_fig = figure;
@@ -71,4 +74,57 @@ if plot_control_output == true
     ylim([-5,5])
     xlim([0,t(end)])
     grid on;  % add a grid
+end
+
+
+
+%% Animate the airbrakes
+if animation_airbrake_position == true
+    figure;
+    ax = axes;
+    clear figure;
+    
+    % Create a rectangle
+    originalVertices = [0 0; 0.1 0; 0.1 -1.5; 0 -1.5];
+    rectangle = patch('Vertices', originalVertices, 'Faces', [1 2 3 4], 'FaceColor', 'b');
+    
+    % Set the axes limits
+    axis([-2 2 -2 2]);
+    axis square;
+    
+    viscircles([0 0], 0.02, 'Color', 'r');
+    
+    % Create a text object for displaying time
+    timeText = text(0, 1.5, '', 'HorizontalAlignment', 'center','FontSize', 14);
+    posText = text(0, 1.25, '', 'HorizontalAlignment', 'center','FontSize', 14);
+    
+    % Animate the rectangle
+    for i = 1:length(t)-1
+        % Start a timer
+        tic;
+        
+        % Convert the angle from degrees to radians
+        theta = deg2rad(airbrake_position_log(i));
+        
+        % Create the rotation matrix
+        R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+        
+        % Apply the rotation matrix to the rectangle vertices
+        rectangle.Vertices = (R * originalVertices')';
+        
+        % Update the plot
+        drawnow;
+        
+        timeText.String = ['Time: ', sprintf('%.2f', t(i))];
+        posText.String = ['Angle: ', sprintf('%.2f', airbrake_position_log(i))];
+        
+        % Compute the time it took to execute the above commands
+        elapsedTime = toc;
+        
+        % Pause for the time difference between frames, adjusted for the elapsed time
+        pauseTime = t(i+1) - t(i) - elapsedTime;
+        if pauseTime > 0
+            pause(pauseTime);
+        end
+end
 end
