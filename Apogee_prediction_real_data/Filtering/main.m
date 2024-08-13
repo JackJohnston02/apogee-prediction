0f07ecb0d836c69eb5898c8d73d2a1a990153020
JackJohnston02 <jackjohnston1472@gmail.com>
2024-08-13 23:47:30 +0100

    particle filter are a pain in the ass

--- Apogee_prediction_real_data/Filtering/main.m
+++ Apogee_prediction_real_data/Filtering/main.m
@@ -19,8 +19,8 @@ filename = 'data/owen.csv';
 filters = ["UKF_constant_acceleration", "UKF_constant_Cb", "EKF_constant_acceleration", "EKF_constant_Cb", "SIRPF_constant_acceleration", "All"];
 
 %% Filter parameters
-sigma_Q = 0.1;
-sigma_Q_Cb = 0.1;
+sigma_Q = 0.01;
+sigma_Q_Cb = 100;
 % For optimal 
     % CCb UKF:
         % sigma_Q = 0.1
@@ -29,8 +29,8 @@ sigma_Q_Cb = 0.1;
         % sigma_Q = 
         % sigma_Q_Cb = 
 
-measurement_sigma_bar = 10%%0.5744578867366569; % Barometer measurement STD
-measurement_sigma_acc = 10%%0.04;%0.006942717204787825; % Accelerometer measurement STD
+measurement_sigma_bar = 0.5744578867366569; % Barometer measurement STD
+measurement_sigma_acc = 0.04;%0.006942717204787825; % Accelerometer measurement STD
 
 for i = 1:length(filters)
     disp(i + " for " + filters(i))
@@ -38,7 +38,6 @@ end
 
 filter_choice = filters(input("Choose Filter:"));
 
-5
 if filter_choice == "All"
     for i = 1:length(filters)-1
         filter_type = filters(i);
